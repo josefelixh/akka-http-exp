65403d4d4bd6a5f89edafbf3f8b15cc1c84e0ea8
josefelixh <josefelixh@gmail.com>
2015-07-21 02:16:51 +0100

    With custom content type marshalling.

--- main-service/src/main/scala/io/github/josefelixh/exp/Service.scala
+++ main-service/src/main/scala/io/github/josefelixh/exp/Service.scala
@@ -1,17 +1,27 @@
 package io.github.josefelixh.exp
 
 import akka.actor.ActorRef
+import akka.http.scaladsl.marshalling._
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.MediaType.Encoding
 import akka.http.scaladsl.server.Directives._
 import akka.util.Timeout
 import io.github.josefelixh.exp.healthcheck._
-import spray.json.DefaultJsonProtocol
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import spray.json._
 import akka.pattern.ask
 import scala.concurrent.ExecutionContextExecutor
 
 case class ServiceStatus(`http-service`: String, `db`: String)
 
 trait Protocols extends DefaultJsonProtocol {
+  import scala.language.implicitConversions
+  implicit def sprayJsonMarshallerConverter[T](writer: RootJsonWriter[T])(implicit printer: JsonPrinter = PrettyPrinter): ToEntityMarshaller[T] =
+    sprayJsonMarshaller[T](writer, printer)
+  implicit def sprayJsonMarshaller[T](implicit writer: RootJsonWriter[T], printer: JsonPrinter = PrettyPrinter): ToEntityMarshaller[T] =
+    sprayJsValueMarshaller compose writer.write
+  implicit def sprayJsValueMarshaller(implicit printer: JsonPrinter = PrettyPrinter): ToEntityMarshaller[JsValue] =
+    Marshaller.StringMarshaller.wrap(ContentType(MediaType.custom("application/vnd.service.v1+json", Encoding.Fixed(HttpCharsets.`UTF-8`))))(printer)
+
   implicit val statusFormat = jsonFormat2(ServiceStatus)
 }
 
--- main-service/src/test/scala/io/github/josefelixh/exp/ServiceSpec.scala
+++ main-service/src/test/scala/io/github/josefelixh/exp/ServiceSpec.scala
@@ -1,15 +1,16 @@
 package io.github.josefelixh.exp
 
+import akka.http.scaladsl.model.MediaType.Encoding
+import akka.http.scaladsl.model.{HttpCharsets, MediaType, ContentType}
+import akka.http.scaladsl.model.headers.RawHeader
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import akka.http.scaladsl.model.StatusCodes._
-import akka.http.scaladsl.model.ContentTypes.`application/json`
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import com.github.tomakehurst.wiremock.client.WireMock.{get, urlEqualTo, aResponse}
 import io.github.josefelixh.exp.healthcheck.Healthy
 import io.github.josefelixh.exp.stubs.{StubbedHttpService, StubbedCassandra}
 import org.scalatest.{Matchers, FlatSpec}
 import org.scassandra.http.client.PrimingRequest
-import spray.json.JsObject
+import spray.json._
 import scala.collection.JavaConversions._
 import scala.concurrent.ExecutionContextExecutor
 
@@ -35,11 +36,10 @@ class ServiceSpec extends FlatSpec
 
     wiremockClient.stubFor(get(urlEqualTo("/service/status")).willReturn(aResponse().withStatus(204)))
 
-    Get("/status") ~> routes ~> check {
+    Get("/status").withHeaders(RawHeader("Accept", "application/vnd.service.v1+json")) ~> routes ~> check {
       status shouldBe OK
-      contentType shouldBe `application/json`
-      responseAs[ServiceStatus] shouldBe ServiceStatus(Healthy, Healthy)
-      responseAs[JsObject] shouldBe JsObject(
+      contentType shouldBe ContentType(MediaType.custom("application/vnd.service.v1+json", Encoding.Fixed(HttpCharsets.`UTF-8`)))
+      responseAs[String].parseJson shouldBe JsObject(
         "http-service" -> Healthy,
         "db" -> Healthy
       )
